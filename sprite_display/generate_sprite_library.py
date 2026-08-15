b40c4c1fc7d09821e499bc8a688dd4b67e253cf4
BlockerLocker <101794628+blockerlocker@users.noreply.github.com>
2026-08-14 20:01:05 -0700

    Updated sprite_display to 26.3-snapshot-8

--- sprite_display/generate_sprite_library.py
+++ sprite_display/generate_sprite_library.py
@@ -7,7 +7,7 @@ if len(sys.argv) > 1:
     MCVERSION = sys.argv[1]
 else:
 #### SET MINECRAFT VERSION MANUALLY HERE ####
-    MCVERSION = "26.3-snapshot-7"
+    MCVERSION = "26.3-snapshot-8"
 
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
